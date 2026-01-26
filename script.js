f8f942f4b56b8b82eb8327e6661610b743e8125c
delirium02 <darknightbs@gmail.com>
2026-01-26 22:01:33 +1100

    added game board object with the board's array

---
+++ script.js
@@ -0,0 +1,15 @@
+
+const gameBoard = {
+    board: ["", "", "", "", "", "", "", "", ""],
+    placeMark: function(position, marker) {
+        this.board[position] = marker;
+    }
+}
+
+const Player = (function (name, marker) {
+    return {name, role};
+});
+
+const gameFlow = {
+
+}
