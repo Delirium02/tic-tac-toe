0b86c406a5033d9c2d4e165ca69b6fe11e73d10c
delirium02 <darknightbs@gmail.com>
2026-01-29 23:08:09 +1100

    Created a forEach loop connecting the HTML elements with the JS logic

--- script.js
+++ script.js
@@ -19,7 +19,6 @@ const player2 = createPlayer("Opp", "O");
 let currentPlayer = player1;
 
 function switchTurn() {
-
   if (currentPlayer == player1) {
     currentPlayer = player2;
   } else {
@@ -27,6 +26,27 @@ function switchTurn() {
   }
 };
 
+gameBoard.placeMark(4, currentPlayer.marker);
+
+const blocks = document.querySelectorAll(".block");
+
+blocks.forEach((block) => {
+  block.addEventListener("click", (e) => {
+    const index = e.target.dataset.index;
+
+    if (gameBoard.board[index] == "") {
+      gameBoard.placeMark(index, currentPlayer.marker);
+
+      e.target.textContent = currentPlayer.marker;
+
+      switchTurn();
+    }
+  });
+});
+
+// const playButton = document.querySelector(".play-button"); --> Not used currently
+
 const gameFlow = {
 
 }
+
