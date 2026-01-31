af910278e8bf639c26cb36266cb21fb8136d2509
delirium02 <darknightbs@gmail.com>
2026-01-31 16:20:50 +1100

    Marker CSS change for more clarity

--- script.js
+++ script.js
@@ -26,8 +26,6 @@ function switchTurn() {
   }
 };
 
-gameBoard.placeMark(4, currentPlayer.marker);
-
 const blocks = document.querySelectorAll(".block");
 
 blocks.forEach((block) => {
@@ -39,10 +37,23 @@ blocks.forEach((block) => {
 
       e.target.textContent = currentPlayer.marker;
 
+      e.target.style.fontSize = "48px";
+      e.target.style.display = "flex";
+      e.target.style.justifyContent = "center";
+      e.target.style.alignItems = "center";
+
       switchTurn();
     }
   });
 });
+
+const winConditions = [
+  [0, 1, 2], [3, 4, 5], [6, 7, 8], // Rows
+  [0, 3, 6], [1, 4, 7], [2, 5, 8], // Columns
+  [0, 4, 8], [2, 4, 6]             // Diagonals
+];
+
+//const win = 
 
 // const playButton = document.querySelector(".play-button"); --> Not used currently
 
